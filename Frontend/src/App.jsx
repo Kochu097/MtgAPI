0176fe485388a91d93afed9663d0d7d8c351c6fc
Maciej Nowak-Kochman <maciej.nowak-kochman@mavoco.com>
2025-07-17 14:30:22 +0200

    missing files

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MTGApiShowcase from './components/MTGApiShowcase';
+
+function App() {
+  return (
+    <div className="App">
+      <MTGApiShowcase />
+    </div>
+  );
+}
+
+export default App;
