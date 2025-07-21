4530e3a9def0fac1d8dfa172b05d924993fe20d5
Maciej Nowak-Kochman <maciej.nowak-kochman@mavoco.com>
2025-07-21 10:14:33 +0200

    Added register/login functionality

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import MTGApiShowcase from './components/MTGApiShowcase';
+import AuthModule from './components/AuthModule';
+import { AuthProvider } from './contexts/AuthContext';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 function App() {
+    const firebaseConfig = {
+        apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+        authDomain: "mtgapi-9f486.firebaseapp.com",
+        projectId: "mtgapi-9f486",
+        storageBucket: "mtgapi-9f486.firebasestorage.app",
+        messagingSenderId: "644685582424",
+        appId: "1:644685582424:web:a028b2433391fef6903a2d",
+        measurementId: "G-DE7MQ0NDWC"
+    };
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+    console.log(app);
   return (
-    <div className="App">
-      <MTGApiShowcase />
-    </div>
+      <AuthProvider>
+          <div className="App">
+              <AuthModule />
+              <MTGApiShowcase />
+          </div>
+      </AuthProvider>
   );
 }
 
